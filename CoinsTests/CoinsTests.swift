8ee6b3968887feb03612c723e6280defbbfedc46
neddoz <kdenno25@gmail.com>
2025-02-27 18:38:24 +0300

    Initial Commit

---
+++ CoinsTests/CoinsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinsTests.swift
+//  CoinsTests
+//
+//  Created by kayeli dennis on 27/02/2025.
+//
+
+import Testing
+@testable import Coins
+
+struct CoinsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
